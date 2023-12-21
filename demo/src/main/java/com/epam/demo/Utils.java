fbe2406e78945fa6edb64b8506ef29aa514b3a94
VLinkus <LinkusVyte@gmail.com>
2023-12-21 14:29:48 +0200

    Maven Fixed

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -1,10 +1,10 @@
 package com.epam.demo;
 
-import org.apache.commons.lang3.math.NumberUtils;
 import java.util.List;
+import com.epam.utils.StringUtils;
 
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
-       return args.stream().allMatch(x -> NumberUtils.isCreatable(x) && NumberUtils.toDouble(x)>0);
+       return args.stream().allMatch(StringUtils::isPositiveNumber);
     }
 }
